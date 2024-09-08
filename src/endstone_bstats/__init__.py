62c72a4bdc3085ee4cd1510e345f20ec5499d517
Vincent <magicdroidx@gmail.com>
2024-09-08 16:48:06 +0100

    feat: add MetricsConfig class

--- src/endstone_bstats/__init__.py
+++ src/endstone_bstats/__init__.py
@@ -1,9 +1,11 @@
 from endstone_bstats._charts import ChartDataError, CustomChart
 from endstone_bstats._charts.pie import SimplePie, AdvancedPie
 from endstone_bstats._metrics import Metrics
+from endstone_bstats._config import MetricsConfig
 
 __all__ = [
     "Metrics",
+    "MetricsConfig",
     "CustomChart",
     "SimplePie",
     "AdvancedPie",

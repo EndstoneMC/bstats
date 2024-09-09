3e4747b14aa3154a966698d3e420915350369ad2
Vincent <magicdroidx@gmail.com>
2024-09-09 16:40:02 +0100

    test: add tests for DrilldownPie

--- src/endstone_bstats/__init__.py
+++ src/endstone_bstats/__init__.py
@@ -1,6 +1,7 @@
 from endstone_bstats._base import MetricsBase
 from endstone_bstats._charts.advanced_pie import AdvancedPie
 from endstone_bstats._charts.custom_chart import CustomChart
+from endstone_bstats._charts.drilldown_pie import DrilldownPie
 from endstone_bstats._charts.simple_pie import SimplePie
 from endstone_bstats._config import MetricsConfig
 from endstone_bstats._errors import ChartDataError
@@ -13,5 +14,6 @@ __all__ = [
     "CustomChart",
     "SimplePie",
     "AdvancedPie",
+    "DrilldownPie",
     "ChartDataError",
 ]

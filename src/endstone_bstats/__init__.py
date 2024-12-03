929312b628a60db4e8e5a6d1c12f90e26296d874
Vincent <magicdroidx@gmail.com>
2024-12-03 15:56:27 +0000

    feat: add MultiLineChart, AdvancedBarChart, SimpleBarChart

--- src/endstone_bstats/__init__.py
+++ src/endstone_bstats/__init__.py
@@ -3,6 +3,9 @@ from endstone_bstats._charts.advanced_pie import AdvancedPie
 from endstone_bstats._charts.custom_chart import CustomChart
 from endstone_bstats._charts.drilldown_pie import DrilldownPie
 from endstone_bstats._charts.simple_pie import SimplePie
+from endstone_bstats._charts.multi_line_chart import MultiLineChart
+from endstone_bstats._charts.simple_bar_chart import SimpleBarChart
+from endstone_bstats._charts.advanced_bar_chart import AdvancedBarChart
 from endstone_bstats._charts.single_line_chart import SingleLineChart
 from endstone_bstats._config import MetricsConfig
 from endstone_bstats._errors import ChartDataError
@@ -17,5 +20,8 @@ __all__ = [
     "AdvancedPie",
     "DrilldownPie",
     "SingleLineChart",
+    "MultiLineChart",
+    "SimpleBarChart",
+    "AdvancedBarChart",
     "ChartDataError",
 ]
